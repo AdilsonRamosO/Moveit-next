b6bf6c99d9d549172a54586472fad7b0bfb4266c
Adilson Ramos <75184157+AdilsonRamosO@users.noreply.github.com>
2021-02-23 21:32:07 -0300

    Moveit-Next

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/Components/Profile.module.css';
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/75184157?s=460&u=a66fc9cfb3a2148ae200ae9dbc119c7ea5120940&v=4" alt="Adilson Ramos"/>
+            <div>
+                <strong>Adilson Ramos</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level 1
+                </p>
+            </div>
+        </div>
+    )
+}
