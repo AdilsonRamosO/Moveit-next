125ecdf1d982a09ee9d30e25d039dbc27e1cf463
Adilson Ramos <75184157+AdilsonRamosO@users.noreply.github.com>
2021-02-26 23:04:12 -0300

    Finalizando a aplicação.

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/Components/Profile.module.css';
 
 export function Profile() {
+
+    const { level } = useContext(ChallengesContext)
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://avatars.githubusercontent.com/u/75184157?s=460&u=a66fc9cfb3a2148ae200ae9dbc119c7ea5120940&v=4" alt="Adilson Ramos"/>
@@ -8,7 +13,7 @@ export function Profile() {
                 <strong>Adilson Ramos</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
